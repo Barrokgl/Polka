330d1c59b8d864a5f4ad7048a1dc41a15d5ba277
Barrokgl <barrokgl@gmail.com>
2016-07-11 14:43:24 +0300

    new version

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,14 @@
 //main javascript document
-var button = document.getElementById('loginCall');
-function Click() {
-    alert('Boom!');
-}
-button.addEventListener("click", Click);
+// var button = document.getElementById('loginCall');
+// function Click() {
+//     alert('Boom!');
+// }
+// button.addEventListener("click", Click);
+
+
+// var imgArray= document.getElementsByClassName('book');
+// var imgSrc = imgArray.getAttribute('src');
+////            var labelArray = document.getElementsByClassName('book-label');
+//            console.log(imgArray);
+////            console.log(imgArray[0].attributes[1].nodeValue);
+// console.log(imgSrc);
